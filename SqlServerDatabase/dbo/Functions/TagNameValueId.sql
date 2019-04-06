8ce331ee923fc6f24e3b43d9050b93e67054a820
Tomasz Jastrzębski <tdjastrzebski@wp.pl>
2019-04-06 10:44:50 +0200

    Update TagNameValueId.sql

--- SqlServerDatabase/dbo/Functions/TagNameValueId.sql
+++ SqlServerDatabase/dbo/Functions/TagNameValueId.sql
@@ -7,10 +7,5 @@ CREATE function [dbo].[TagNameValueId]
 returns uniqueidentifier
 as
 begin
-	--@declare @text as char(64) = convert(char(64), hashbytes('SHA2_256', @Name + ':' + @Value), 2)
-	--return convert(uniqueidentifier,
-	--   convert(binary(8), convert(bigint, convert(binary(8), substring(@text, 1, 16), 2)) ^ convert(bigint, convert(binary(8), substring(@text, 17, 16), 2))) +
-	--   convert(binary(8), convert(bigint, convert(binary(8), substring(@text, 33, 16), 2)) ^ convert(bigint, convert(binary(8), substring(@text, 49, 16), 2)))
-	--)
 	return convert(uniqueidentifier, hashbytes('MD5', lower(@Name) + ':' + lower(@Value)), 2)
 end
